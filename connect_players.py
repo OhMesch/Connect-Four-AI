e6b92798928c4f4e9be4f7453bcbb49606cb18ac
OhMesch <mze5111@gmail.com>
2017-09-07 23:21:21 -0400

    Begun monte carlo implimentation

--- connect_players.py
+++ connect_players.py
@@ -3,10 +3,9 @@ import time
 import connect_engine
 
 class Player():
-    def __init__(self,color,VIS):
+    def __init__(self, color, engine):
         self.color = color
-        self.engine = connect_engine.Engine()
-        self.VIS = VIS
+        self.engine = engine
 
     def get_color(self):
         return(self.color)
@@ -18,33 +17,66 @@ class Player():
         self.lastX = x
         self.lastY = y
 
+    def get_type(self):
+        raise Exception('not implemented')
+
+
 class AiRand(Player):
-    def __init__(self,color,VIS):
-        super().__init__(color,VIS)
+    def __init__(self,color, engine):
+        super().__init__(color, engine)
 
-    def get_move(self,board):
-        choice = 7
-        while not self.engine.valid_move(board,choice):
-            choice = random.randint(0,6)
-            if self.engine.valid_move(board,choice):
-                if self.VIS:
-                    time.sleep(1)
-                return(choice)
+    def get_move(self, moves):
+        return(random.choice(moves))
 
-    def is_human(self):
-        return(False)
+    def get_type(self):
+        return('random')
+
 
 class Human(Player):
-    def __init__(self,color,VIS):
-        super().__init__(color,VIS)
+    def __init__(self, color, engine, renderer):
+        super().__init__(color, engine)
+        self.renderer = renderer
 
-    def get_move(self,board,win,maxWidth):
-        choice = 7
-        while not self.engine.valid_move(board,choice):
-            alley=(win.getMouse()).getX()
-            choice=int(alley//(maxWidth/7))
-            if self.engine.valid_move(board,choice):
+    def get_move(self, moves):
+        win = self.renderer.get_window()
+        maxWidth = self.renderer.get_width()
+
+        board = self.engine.get_board()
+
+        while True:
+            alley = (win.getMouse()).getX()
+            choice = int(alley // (maxWidth / 7))
+
+            if choice in moves:
                 return(choice)
+            print('pick a real move')
 
-    def is_human(self):
-        return(True)
+    def get_type(self):
+        return('human')
+
+
+class AiMonte(Player):
+    def __init__(self, color, engine, samples):
+        super().__init__(color, engine)
+        self.samples = samples
+
+    def get_move(self, moves):
+        children = len(moves)
+        curr_samples = self.samples
+        samples_per_child = curr_samples//children
+
+        while samples_per_child != 0:
+            curr_samples += 1
+            samples_per_child = curr_samples//children
+
+        print('getting move with', curr_samples)
+
+        for child in moves:
+            self.hard_rollout(child,samples_per_child)
+
+
+    def hard_rollout(self,move,samples):
+
+
+    def get_type(self):
+        return('monte')
